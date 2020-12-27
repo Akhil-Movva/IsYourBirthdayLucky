20052d6d625b8db74f920d5fc230c7ff41d1367f
Akhil-Movva <akhilmovva@gmail.com>
2020-12-27 23:41:18 +0530

    feat- dynamic rendering of graphics.

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var btn=document.querySelector("#btn");
 
 
 
+
 function clickHandler(){
    var dob=date.value;
    dob=Number(dob.slice(8,dob.length));
    var num=number.value;
    var rem,sum=0
+   var luckyImg,message;
 
   console.log(dob);
   console.log(num);  
@@ -18,12 +20,24 @@ while(dob>0)
    sum=sum+rem;
    dob=Math.floor(dob/10);
  }
+ luckyImg=document.createElement("img");
+ luckyImg.style.height="200px";
+  luckyImg.style.width="400px";
+  luckyImg.style.margin="auto";
+ document.getElementById("graphics").appendChild(luckyImg);
 
  if(sum%num==0)
-   console.log("\nYour birthday is lucky!");
+   {
+       
+       luckyImg.src="https://media.giphy.com/media/SUQzS3BEeUhLnIhLtD/giphy.gif";
+       
+  }
  else
-   console.log("\nYour birthday is not lucky:(");
+   {
+     luckyImg.src="https://media.giphy.com/media/WpaVhEcp3Qo2TjwyI1/giphy.gif";
+   }
    
+
 btn.removeEventListener("click",clickHandler);   
    
 }

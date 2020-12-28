196396ba6b3370dba6025f90f9e2e52650560d8f
Akhil-Movva <akhilmovva@gmail.com>
2020-12-28 19:45:11 +0530

    feat - added dynamic mesage rendering.

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
-var date=document.querySelector("#date");
-var number=document.querySelector("#number");
-var btn=document.querySelector("#btn");
+var date = document.querySelector("#date");
+var number = document.querySelector("#number");
+var btn = document.querySelector("#btn");
 
 
-
-
-function clickHandler(){
-   var dob=date.value;
-   dob=Number(dob.slice(8,dob.length));
-   var num=number.value;
-   var rem,sum=0
-   var luckyImg,message;
+function clickHandler() {
+  var dob = date.value;
+  dob = Number(dob.slice(8, dob.length));
+  var num = number.value;
+  var rem, sum = 0
+  var luckyImg, message;
 
   console.log(dob);
-  console.log(num);  
-while(dob>0)
- {
-   rem=dob%10;
-   sum=sum+rem;
-   dob=Math.floor(dob/10);
- }
- luckyImg=document.createElement("img");
- luckyImg.style.height="200px";
-  luckyImg.style.width="400px";
-  luckyImg.style.margin="auto";
- document.getElementById("graphics").appendChild(luckyImg);
-
- if(sum%num==0)
-   {
-       
-       luckyImg.src="https://media.giphy.com/media/SUQzS3BEeUhLnIhLtD/giphy.gif";
-       
+  console.log(num);
+  while (dob > 0) {
+    rem = dob % 10;
+    sum = sum + rem;
+    dob = Math.floor(dob / 10);
   }
- else
-   {
-     luckyImg.src="https://media.giphy.com/media/WpaVhEcp3Qo2TjwyI1/giphy.gif";
-   }
-   
+  luckyImg = document.createElement("img");
+  luckyImg.style.height = "200px";
+  luckyImg.style.width = "400px";
+  luckyImg.style.margin = "auto";
+  document.getElementById("graphics").appendChild(luckyImg);
 
-btn.removeEventListener("click",clickHandler);   
-   
+
+  if (sum % num == 0) {
+
+    luckyImg.src = "https://media.giphy.com/media/SUQzS3BEeUhLnIhLtD/giphy.gif";
+    message = "Yayy...your birthday is lucky!!!";
+  } else {
+    luckyImg.src = "https://media.giphy.com/media/WpaVhEcp3Qo2TjwyI1/giphy.gif";
+    message = "Oops...your birthday is not lucky:("
+  }
+ 
+  var text = document.getElementById("message");
+  text.innerText = message;
+  text.style.textAlign = "center";
+  text.style.fontSize = "2rem";
+  text.style.color = "#1F2937";
+
+  btn.removeEventListener("click", clickHandler);
+
 }
 
-
-btn.addEventListener("click",clickHandler);
-
+btn.addEventListener("click", clickHandler);
